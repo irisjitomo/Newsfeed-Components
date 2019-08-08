e3d37ba2ff091316e12add1ffaa409222deb4c2c
Jonathan Taylor <jonyonson@icloud.com>
2019-08-08 12:40:47 -0500

    Revert "Iris jitomo"

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,6 +1,13 @@
 /* This is the data we will be using, study it but don't change anything, yet. */
 
-let menuItems = [ 'Students', 'Faculty', "What's New", 'Tech Trends', 'Music', 'Log Out' ];
+let menuItems = [
+  'Students',
+  'Faculty',
+  "What's New",
+  'Tech Trends',
+  'Music',
+  'Log Out'
+];
 
 /* 
 
@@ -26,39 +33,3 @@ let menuItems = [ 'Students', 'Faculty', "What's New", 'Tech Trends', 'Music', '
   Step 6: add the menu component to the DOM.
   
 */
-
-window.addEventListener('load', (e) => {
-  const menuButton = document.querySelector('.header');
-  
-  menuButton.append(makeComponent(menuItems))
-
-	function makeComponent(arr) {
-    // new elemets //
-		const menuDiv = document.createElement('div');
-		const ulItem = document.createElement('ul');
-		
-		// structure //
-    menuDiv.appendChild(ulItem);
-
-    arr.forEach(element => {
-      const liItem = document.createElement('li');
-      ulItem.appendChild(liItem);
-      liItem.textContent = element;
-      console.log(element)
-    })
-
-    // set content // 
-    menuDiv.classList.add('menu');
-    
-    
-
-    // liItem.textContent = object
-		//
-    const menuButton = document.querySelector('.menu-button');
-		menuButton.addEventListener('click', () => {
-			menuDiv.classList.toggle('menu--open');
-		});
-
-		return menuDiv;
-	}
-});
